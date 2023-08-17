8ee9074ae2ee81bb5be8624311c6e79341161245
Promesis <3149154722@qq.com>
2023-08-17 22:05:28 +0800

    new file:   Program.cs 	new file:   nullable-type.csproj

---
+++ csharp/language-features/nullable-type/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+SomeReferenceType? 
+    some_variable = new(),
+    some_null_variable = null;
+
+if (some_variable != null)
+    some_variable.SomeAttribute = 0;
+
+int x = some_variable?.SomeAttribute ?? -1;
+int y = some_null_variable?.SomeAttribute ?? -1;
+
+Console.WriteLine($"x = some_variable?.SomeAttribute ?? -1= {x}");
+Console.WriteLine($"y = some_null_variable?.SomeAttribute ?? -1 = {y}");
+
+public class SomeReferenceType
+{
+    public int SomeAttribute {get; set;}
+}
